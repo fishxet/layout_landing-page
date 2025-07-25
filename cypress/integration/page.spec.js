c53dc0622c0de2e4c46c39aa62f1beb6cf67dcfa
Leonid Markevych <ggfhappinesss@gmail.com>
2025-07-26 00:05:47 +0300

    finished sections top-bar, about-us, compare-bikes, the details

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,7 +1,9 @@
 'use strict';
 
 // TODO: edit it
+// eslint-disable-next-line no-undef
 describe('Page', () => {
+  // eslint-disable-next-line no-undef
   it('should be visitable', () => {
     // eslint-disable-next-line no-undef
     cy.visit('/');
